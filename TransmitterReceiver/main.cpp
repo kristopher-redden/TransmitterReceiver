5d05c2575fe4ebf7f30b8f57bcf0600e4f63d558
kristopher-redden <reddenkris@gmail.com>
2017-11-17 13:52:06 -0500

    Adjusted error handling.

--- TransmitterReceiver/main.cpp
+++ TransmitterReceiver/main.cpp
@@ -13,13 +13,20 @@ int main(int argc, char* argv[]) {
     string file2 = "";
     string file3 = "";
 
+    if (argc != 4)
+    {
+        cerr << "The number of input parameters is not correct, please enter the command, hostname and file to read/write from/to.";
+        exit(3);
+    }
+
     //Let port number be a parameter that you can pass in.
     if (command == "-th")
     {
-        string hostname = argv[2];
-        string fileToReadFrom = argv[3];
         try
         {
+            string hostname = argv[2];
+            string fileToReadFrom = argv[3];
+
             ApplicationLayer al;
             al.CommandTHam(hostname, fileToReadFrom, true, true);
         }
@@ -40,39 +47,45 @@ int main(int argc, char* argv[]) {
     }
     else if (command == "-tcrc")
     {
-        string hostname = argv[2];
-        string fileToReadFrom = argv[3];
         try
         {
+            string hostname = argv[2];
+            string fileToReadFrom = argv[3];
+
             ApplicationLayer al;    //              ham,  clientTrans
             al.CommandTCrc(hostname, fileToReadFrom, false, true);
         }
         catch (int exception)
         {
             string message;
+            if (exception == 3)
+            {
+                message = "Transmitted With Error: Input file or output file is invalid.";
+            }
             cerr << message;
             exit(exception);
         }
     }
     else if (command == "-rh")
     {
-        string hostname = argv[2];
-        string fileToWriteTo = argv[3];
         try
         {
+            string hostname = argv[2];
+            string fileToWriteTo = argv[3];
+
             ApplicationLayer al;    //              ham, clientTrans
             al.CommandRHam(hostname, fileToWriteTo, true, false);
         }
         catch (int exception)
         {
             string message;
-            if (exception == 3)
+            if (exception == 1)
             {
-                message = "Transmitter With Error: Input file or output file is invalid.";
+                message = "Receiver With Error: First or last character is not a SYN.";
             }
-            else if (exception == 4)
+            else if (exception == 2)
             {
-                message = "Transmitter With Error: File that was written to has no length.";
+                message = "Receiver With Error: Char parity was not correct.";
             }
             cerr << message;
             exit(exception);
@@ -80,27 +93,28 @@ int main(int argc, char* argv[]) {
     }
     else if (command == "-rcrc")
     {
-        string hostname = argv[2];
-        string fileToWriteTo = argv[3];
         try
         {
+            string hostname = argv[2];
+            string fileToWriteTo = argv[3];
+
             ApplicationLayer al;    //              ham,  clientTrans
             al.CommandRCrc(hostname, fileToWriteTo, false, false);
         }
         catch (int exception)
         {
             string message;
-            if (exception == 3)
+            if (exception == 1)
             {
-                message = "Transmitter With Error: Input file or output file is invalid.";
+                message = "Receiver With Error: First or last character is not a SYN.";
             }
-            else if (exception == 4)
+            else if (exception == 2)
             {
-                message = "Transmitter With Error: File that was written to has no length.";
+                message = "Receiver With Error: Char parity was not correct.";
             }
             else if (exception == 99)
             {
-                message = "Transmitter With Error: CRC did not return a 0 remainder.";
+                message = "Receiver With Error: CRC did not return a 0 remainder.";
             }
             cerr << message;
             exit(exception);
